43510a25fe94765ec5be9e3cf414979c1fe860c1
Marvin Kadoyo <kadoyomarvin@gmail.com>
2025-05-02 09:30:51 +0300

    Complete Jest test suite for isPalindrome with normal and edge cases

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,28 @@
 // Your tests here
+const { isPalindrome } = require('../utils');
+
+describe("isPalindrome", () => {
+  it("should return true for 'racecar'", () => {
+    expect(isPalindrome("racecar")).toBe(true);
+  });
+
+  it("should return false for 'car'", () => {
+    expect(isPalindrome("car")).toBe(false);
+  });
+
+  it("should return true for mixed case palindromes like 'RaceCar'", () => {
+    expect(isPalindrome("RaceCar")).toBe(true);
+  });
+
+  it("should return false for an empty string", () => {
+    expect(isPalindrome("")).toBe(false);
+  });
+
+  it("should throw an error if input contains non-alphabetic characters", () => {
+    expect(() => isPalindrome("race1car")).toThrow("Input must contain only alphabetic characters");
+  });
+
+  it("should throw an error if input is not a string", () => {
+    expect(() => isPalindrome(12321)).toThrow("Input must be a string");
+  });
+});
